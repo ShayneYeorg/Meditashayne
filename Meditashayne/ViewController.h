7c4d0e06068c273c6d0b62abfe4a69bef306038b
ShayneYeorg <mercury10@qq.com>
2016-02-01 10:02:42 +0800

    getting start

---
+++ Meditashayne/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Meditashayne
+//
+//  Created by 杨淳引 on 16/2/1.
+//  Copyright © 2016年 shayneyeorg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Meditashayne/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Meditashayne
+//
+//  Created by 杨淳引 on 16/2/1.
+//  Copyright © 2016年 shayneyeorg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
